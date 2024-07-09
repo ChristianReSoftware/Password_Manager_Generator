13dad1ba047d4c58573ea3b899cd7bf8840794a3
ChristianReSoftware <christianreher@rocketmail.com>
2024-07-09 14:17:37 +0200

    set up the Main Window

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Password_Management;
+﻿using System.Runtime.CompilerServices;
+
+namespace Password_Management;
 
 partial class Form1
 {
@@ -7,10 +9,17 @@ partial class Form1
     /// </summary>
     private ToolStrip toolStrip1;
     private System.ComponentModel.IContainer components = null;
-    private System.Windows.Forms.Label label1;
-    private System.Windows.Forms.Label labelOutput;
+    private System.Windows.Forms.Label labelWebsite;
     private System.Windows.Forms.Button Search;
+    private System.Windows.Forms.Button buttonShow;
     private TextBox textBoxWebsite;
+    private TextBox textBoxAusgabe_1;
+    private TextBox textBoxAusgabe_2;
+
+    private string filePath = "";
+
+    
+    
 
 
     /// <summary>
@@ -35,8 +44,9 @@ partial class Form1
     private void InitializeComponent()
     {
         this.components = new System.ComponentModel.Container();
+        this.BackColor = System.Drawing.Color.White;
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
+        this.ClientSize = new System.Drawing.Size(500, 300);
         this.Text = "Password Managementsystem";
 
         // ToolStrip erstellen
@@ -45,44 +55,73 @@ partial class Form1
         
 
         // Schaltfläche zur ToolStrip hinzufügen
+        ToolStripButton buttonOpenDialog = new ToolStripButton();
+        buttonOpenDialog.Text = "OpenFile";
+        buttonOpenDialog.Click += OpenFileButton_Click;
+        toolStrip1.Items.Add(buttonOpenDialog);
+
         ToolStripButton buttonNew = new ToolStripButton();
         buttonNew.Text = "New Password";
-        buttonNew.Click += OpenSecondaryFormButton_Click;
+        buttonNew.Click += OpenAddFormButton_Click;
         toolStrip1.Items.Add(buttonNew);
 
         ToolStripButton buttonChange = new ToolStripButton();
         buttonChange.Text = "Change Password";
-        buttonChange.Click += ButtonNew_Click;
+        buttonChange.Click += OpenChangeFormButton_Click;
         toolStrip1.Items.Add(buttonChange);
-
-
-        // Label erstellen und initialisieren
-        labelOutput = new Label();
-        labelOutput.Text = "Website: example.com, Password: ";
-        labelOutput.Location = new System.Drawing.Point(50, 200);
-        labelOutput.Size = new System.Drawing.Size(500, 300);
 
         // Button erstellen
         Search = new Button();
         Search.Text = "Search";
-        Search.Location = new System.Drawing.Point(50, 50);
+        Search.Location = new System.Drawing.Point(100, 100);
         Search.Size = new System.Drawing.Size(150, 50);
+        Search.Click += ButtonShowPassowrd_Click;
+
+        buttonShow = new Button();
+        buttonShow.Text = "Show Pass";
+        buttonShow.Location = new System.Drawing.Point(300, 100);
+        buttonShow.Size = new System.Drawing.Size(150, 50);
+        buttonShow.Click += ButtonShowAll_Click;
+
+        // Label erstellen 
+        labelWebsite = new System.Windows.Forms.Label();
+        labelWebsite.Location = new System.Drawing.Point(10,50);
+        labelWebsite.Size = new System.Drawing.Size(90,50);
+        labelWebsite.Text = "Website";
 
         // Textbox erstellen
         textBoxWebsite = new System.Windows.Forms.TextBox();
-        textBoxWebsite.Location = new System.Drawing.Point(50, 100);
-        textBoxWebsite.Size = new System.Drawing.Size(350, 100);
-        textBoxWebsite.KeyDown += buttonEnterShowPassowrd_Click; // Ereignishandler für KeyDown-Ereignis hinzufügen
+        textBoxWebsite.Location = new System.Drawing.Point(100, 50);
+        textBoxWebsite.Size = new System.Drawing.Size(200, 50);
+        textBoxWebsite.KeyDown += ButtonEnterShowPassowrd_Click; // Ereignishandler für KeyDown-Ereignis hinzufügen
+
+        textBoxAusgabe_1 = new System.Windows.Forms.TextBox();
+        textBoxAusgabe_1.ReadOnly = true;
+        textBoxAusgabe_1.BorderStyle = BorderStyle.None;
+        textBoxAusgabe_1.BackColor = System.Drawing.Color.White;
+        textBoxAusgabe_1.Location = new System.Drawing.Point(10, 170);
+        textBoxAusgabe_1.Size = new System.Drawing.Size(200, 50);
+        textBoxAusgabe_1.Text = ($"Website:");
+
+        textBoxAusgabe_2 = new System.Windows.Forms.TextBox();
+        textBoxAusgabe_2.ReadOnly = true;
+        textBoxAusgabe_2.BorderStyle = BorderStyle.None;
+        textBoxAusgabe_2.BackColor = System.Drawing.Color.White;
+        textBoxAusgabe_2.Location = new System.Drawing.Point(10, 200);
+        textBoxAusgabe_2.Size = new System.Drawing.Size(400, 50);
+        textBoxAusgabe_2.Text = ("Password:");
+        
 
         // Addd to controls
         this.Controls.Add(toolStrip1);
-        this.Controls.Add(labelOutput);
         this.Controls.Add(Search);
+        this.Controls.Add(buttonShow);
         this.Controls.Add(textBoxWebsite);
-        this.Controls.Add(labelOutput);
+        this.Controls.Add(textBoxAusgabe_1);
+        this.Controls.Add(textBoxAusgabe_2);
+        this.Controls.Add(labelWebsite);
 
-        // Ereignishandler hinzufügen
-        Search.Click += buttonShowPassowrd_Click;
+        
     }
 
     #endregion
--- Form1.cs
+++ Form1.cs
@@ -2,40 +2,91 @@ namespace Password_Management;
 
 public partial class Form1 : Form
 {
+    public PasswordSaveWrite passwordSaveWrite = new PasswordSaveWrite();
+    AddForm addForm;
+    ChangePassForm changePassForm;
+    int oldLength;
     public Form1()
     {
         InitializeComponent();
+        this.Load += Form1_Load;
     }
-    //Function to show Passowrod
-    private void buttonShowPassowrd_Click(object sender, EventArgs e)
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        // Hier wird das Auswählen der Datenbank direkt nach dem start der App gestartet
+        ChoseDataBase();
+        passwordSaveWrite.setLists(passwordSaveWrite.getData(filePath));
+        oldLength = passwordSaveWrite.getLength();
+    }
+    // Function to show Passowrod
+    private void ButtonShowPassowrd_Click(object sender, EventArgs e)
     {
         string ausgabe = textBoxWebsite.Text;
+        string ausgabe2= passwordSaveWrite.SearchPass(ausgabe, filePath);
         // Text im Label ändern, wenn der Button geklickt wird
-        labelOutput.Text = "Website: " + ausgabe + ", Password: Pass321";
+        textBoxAusgabe_1.Text = $"Website: {ausgabe}";
+        textBoxAusgabe_2.Text = $"Password: {ausgabe2}";
     }
-
-    private void buttonEnterShowPassowrd_Click(object sender, KeyEventArgs e)
+    private void ButtonEnterShowPassowrd_Click(object sender, KeyEventArgs e)
     {
         // Überprüfen, ob die Eingabetaste (Enter) gedrückt wurde
         if (e.KeyData == Keys.Enter)
         {
             string ausgabe = textBoxWebsite.Text;
             // Text im Label ändern, wenn der Button geklickt wird
-            labelOutput.Text = "Website: " + ausgabe + ", Password: Pass321";
+            textBoxAusgabe_1.Text = $"Website: {ausgabe}";
+            textBoxAusgabe_2.Text = $"Password: Pass321";
         }
 
+    }
+    private void ButtonShowAll_Click(object sender, EventArgs e)
+    {
+        passwordSaveWrite.showPass();
     }
     private void ButtonNew_Click(object sender, EventArgs e)
     {
         // Aktion ausführen, wenn die Schaltfläche "Neu" geklickt wird
         MessageBox.Show("Neues Element wurde erstellt!");
     }
-
-    private void OpenSecondaryFormButton_Click(object sender, EventArgs e)
+    private void OpenChangeFormButton_Click(object sender, EventArgs e)
     {
-        // Neue Form öffnen
-        SecondaryForm secondaryForm = new SecondaryForm();
-        secondaryForm.Show();
+        this.changePassForm = new ChangePassForm(passwordSaveWrite,filePath);
+        changePassForm.Show();
+    }
+    private void OpenAddFormButton_Click(object sender, EventArgs e)
+    {
+        if (filePath != "")
+        {
+            // Neue Form öffnen
+            this.addForm = new AddForm(passwordSaveWrite, filePath, oldLength);
+            addForm.Show();
+        }
+        else
+        {
+            MessageBox.Show("Need to Select the database");
+        }
+
+    }
+    private void OpenFileButton_Click(object sender, EventArgs e)
+    {
+        ChoseDataBase();
     }
 
+    private void ChoseDataBase()
+    {
+        // OpenFileDialog erstellen
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        // Filter festlegen (optional)
+        openFileDialog.Filter = "Textdateien (*.txt)|*.txt|CSVdateien (*.csv)|*.csv";
+
+        // Dialog anzeigen und Ergebnis prüfen
+        if (openFileDialog.ShowDialog() == DialogResult.OK)
+        {
+            // Gewählte Datei verarbeiten
+            filePath = openFileDialog.FileName;
+            // Debug
+            // MessageBox.Show("Gewählte Datenbank: " + filePath);
+        }
+    }
 }
